5c00f884f53ecb48f324c58d4d025498e1f389cb
DESKTOP-1T14MEC\X555L <eer87912@gmail.com>
2022-07-28 16:13:21 +0800

    pengsuandaesop

--- 7js/js/main.js
+++ 7js/js/main.js
@@ -1,10 +1,10 @@
 
 //OnClick
 function display(){
-    document.getElementById('show').innerHTML='<h2>PENGPENG!</h2>';
+    document.getElementById('show').innerHTML='<h4>My name is Pengsu.</h4>';
 }
 function blue(){
-    document.getElementById('show').innerHTML='<h2 style=color:blue;>PENGPENG!</h2>';
+    document.getElementById('show').innerHTML='<h4 style=color:##D1844B;>Aug 8th <3</h4>';
 
 }
 function erase(){
@@ -23,6 +23,21 @@ function select(){
     document.getElementById("show").innerHTML="你選擇的是:"+ x ;
     document.getElementById("image").src = "img/"+ x +".png"
 
-
-
 }
+
+// OnMouseOver
+
+function show1() {img1.style.display="block";}
+function noshow1() {img1.style.display="none";}
+
+
+function show2() {img2.style.display="block";}
+function noshow2() {img2.style.display="none";}
+
+
+function show3() {img3.style.display="block";}
+function noshow3() {img3.style.display="none";}
+
+
+function show4() {img4.style.display="block";}
+function noshow4() {img4.style.display="none";}
